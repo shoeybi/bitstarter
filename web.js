d5be08125c8effc99f529f0f5386ace84237e22f
Mohammad <shoeybim@gmail.com>
2013-07-15 17:59:05 +0000

    added index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs')
+var buf = fs.readFileSync('index.html')
+var output = buf.toString()
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
